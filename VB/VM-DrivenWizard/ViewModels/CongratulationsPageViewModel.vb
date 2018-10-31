7c9a0b8701256dd63468d98ff1740c2ed85e83a7
DevExpressExampleBot <fakeEmail>
2018-11-01 02:37:46 +0300

    Source auto update [skip ci]

---
+++ VB/VM-DrivenWizard/ViewModels/CongratulationsPageViewModel.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.POCO
+
+Namespace VM_DrivenWizard.ViewModels
+    Public Class CongratulationsPageViewModel
+        Inherits WizardViewModelBase
+        Implements ISupportWizardFinishCommand
+
+        Protected Sub New()
+            ShowBack = True
+            ShowCancel = True
+            ShowFinish = True
+            AllowBack = True
+        End Sub
+        Public ReadOnly Property CanFinish() As Boolean
+            Get
+                Return True
+            End Get
+        End Property
+
+        Public Sub OnFinish(ByVal e As CancelEventArgs)
+            Me.GetService(Of IMessageBoxService)().ShowMessage("Thank you for completing this WPF feature tour!", "WPF Tour", MessageButton.OK, MessageIcon.Exclamation)
+        End Sub
+        Protected Overrides Function GetCanCancel() As Boolean
+            Return False
+        End Function
+    End Class
+End Namespace
---
+++ VB/VM-DrivenWizard/ViewModels/MainWindowViewModel.vb
@@ -0,0 +1,43 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Linq
+Imports System.Text
+Imports System.Threading
+Imports System.Threading.Tasks
+Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.POCO
+
+
+Namespace VM_DrivenWizard.ViewModel
+    Public Class MainWindowViewModel
+        Public Shared Function Create() As MainWindowViewModel
+            Return ViewModelSource.Create(Function() New MainWindowViewModel())
+        End Function
+
+        Protected Sub New()
+            Model = New Model()
+        End Sub
+
+        Private privateText As String
+        Public Overridable Property Text() As String
+            Get
+                Return privateText
+            End Get
+            Protected Set(ByVal value As String)
+                privateText = value
+            End Set
+        End Property
+
+        Public Sub ShowDialog()
+            Dim wizardResult = Me.GetRequiredService(Of IDialogService)().ShowDialog(MessageButton.OKCancel, "Wizard", Me).ToString()
+            Dim song = Model.Song
+            Text = "Wizard result: " & wizardResult & (If(String.IsNullOrEmpty(song), String.Empty, ", you choose " & Model.Song))
+        End Sub
+
+        Private Property Model() As Model
+        Public Sub ViewLoaded()
+            Me.GetRequiredService(Of IWizardService)().Navigate("WelcomePage", Nothing, Model, Me)
+        End Sub
+    End Class
+End Namespace
