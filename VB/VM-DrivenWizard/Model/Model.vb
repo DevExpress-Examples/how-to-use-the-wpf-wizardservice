b3bec87429533fac049a3c89172a2b13a0b5addb
DevExpressExampleBot <fakeEmail>
2022-12-08 18:53:46 +0400

    Source auto update [skip ci]

--- VB/VM-DrivenWizard/Model/Model.vb
+++ VB/VM-DrivenWizard/Model/Model.vb
@@ -1,12 +1,7 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports DevExpress.Mvvm.POCO
-
 Namespace VM_DrivenWizard
+
     Public Class Model
-        Public Property Song() As String
+
+        Public Property Song As String
     End Class
 End Namespace
--- VB/VM-DrivenWizard/ViewModels/PlayTunePageViewModel.vb
+++ VB/VM-DrivenWizard/ViewModels/PlayTunePageViewModel.vb
@@ -1,13 +1,10 @@
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.POCO
 
 Namespace VM_DrivenWizard.ViewModels
+
     Public Class PlayTunePageViewModel
         Inherits WizardViewModelBase
         Implements ISupportWizardNextCommand, ISupportWizardFinishCommand
@@ -15,6 +12,7 @@ Namespace VM_DrivenWizard.ViewModels
         Public Shared Function Create() As PlayTunePageViewModel
             Return ViewModelSource.Create(Function() New PlayTunePageViewModel())
         End Function
+
         Protected Sub New()
             ShowBack = True
             ShowCancel = True
@@ -22,12 +20,14 @@ Namespace VM_DrivenWizard.ViewModels
             AllowBack = True
             AllowCancel = True
         End Sub
-        Public ReadOnly Property Header() As String
+
+        Public ReadOnly Property Header As String
             Get
                 Return "Step 2 - Play a tune"
             End Get
         End Property
-        Public ReadOnly Property Description() As String
+
+        Public ReadOnly Property Description As String
             Get
                 Return "To make this demo more entertaining, we would like to play a tune for you. Simple choose your favorite track."
             End Get
@@ -35,22 +35,24 @@ Namespace VM_DrivenWizard.ViewModels
 
         Public Sub Play()
             Dim text As String = "Sorry, but we don't have that song in our library..." & Environment.NewLine
-            text &= "But we are agree with you that ""{0}"" is an exellent choice."
+            text += "But we are agree with you that ""{0}"" is an exellent choice."
             text = String.Format(text, Model.Song)
-            Me.GetService(Of IMessageBoxService)().ShowMessage(text, "Wizard", MessageButton.OK, MessageIcon.Information)
+            GetService(Of IMessageBoxService).ShowMessage(text, "Wizard", MessageButton.OK, MessageIcon.Information)
         End Sub
+
         Public Function CanPlay() As Boolean
             Return Model IsNot Nothing AndAlso Not String.IsNullOrEmpty(Model.Song)
         End Function
-        Public Overridable Property Song() As String
 
-        Public ReadOnly Property CanGoForward() As Boolean Implements ISupportWizardNextCommand.CanGoForward
+        Public Overridable Property Song As String
+
+        Public ReadOnly Property CanGoForward As Boolean Implements ISupportWizardNextCommand.CanGoForward
             Get
                 Return CanPlay()
             End Get
         End Property
 
-        Public ReadOnly Property CanFinish() As Boolean Implements ISupportWizardFinishCommand.CanFinish
+        Public ReadOnly Property CanFinish As Boolean Implements ISupportWizardFinishCommand.CanFinish
             Get
                 Return True
             End Get
@@ -61,10 +63,11 @@ Namespace VM_DrivenWizard.ViewModels
         End Sub
 
         Public Sub OnGoForward(ByVal e As CancelEventArgs) Implements ISupportWizardNextCommand.OnGoForward
-            Me.GetRequiredService(Of IWizardService)().Navigate("CongratulationsPage", Model, Me)
+            GetRequiredService(Of IWizardService)().Navigate("CongratulationsPage", Model, Me)
         End Sub
+
         Public Sub OnFinish(ByVal e As CancelEventArgs) Implements ISupportWizardFinishCommand.OnFinish
-            Me.GetService(Of IMessageBoxService)().ShowMessage("You have finished the tour.", "WPF Tour", MessageButton.OK, MessageIcon.Exclamation)
+            GetService(Of IMessageBoxService)().ShowMessage("You have finished the tour.", "WPF Tour", MessageButton.OK, MessageIcon.Exclamation)
         End Sub
     End Class
 End Namespace
