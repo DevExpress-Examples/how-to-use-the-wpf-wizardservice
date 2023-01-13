b4ea6326371d6f386aa93b46629a8e5668aa6e3e
DevExpressExampleBot <fakeEmail>
2023-01-13 13:38:13 +0400

    Source auto update [skip ci]

--- VB/VM-DrivenWizard/ViewModels/CongratulationsPageViewModel.vb
+++ VB/VM-DrivenWizard/ViewModels/CongratulationsPageViewModel.vb
@@ -1,13 +1,9 @@
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.POCO
 
 Namespace VM_DrivenWizard.ViewModels
+
     Public Class CongratulationsPageViewModel
         Inherits WizardViewModelBase
         Implements ISupportWizardFinishCommand
@@ -18,15 +14,17 @@ Namespace VM_DrivenWizard.ViewModels
             ShowFinish = True
             AllowBack = True
         End Sub
-        Public ReadOnly Property CanFinish() As Boolean Implements ISupportWizardFinishCommand.CanFinish
+
+        Public ReadOnly Property CanFinish As Boolean Implements ISupportWizardFinishCommand.CanFinish
             Get
                 Return True
             End Get
         End Property
 
         Public Sub OnFinish(ByVal e As CancelEventArgs) Implements ISupportWizardFinishCommand.OnFinish
-            Me.GetService(Of IMessageBoxService)().ShowMessage("Thank you for completing this WPF feature tour!", "WPF Tour", MessageButton.OK, MessageIcon.Exclamation)
+            GetService(Of IMessageBoxService)().ShowMessage("Thank you for completing this WPF feature tour!", "WPF Tour", MessageButton.OK, MessageIcon.Exclamation)
         End Sub
+
         Protected Overrides Function GetCanCancel() As Boolean
             Return False
         End Function
--- VB/VM-DrivenWizard/ViewModels/WelcomePageViewModel.vb
+++ VB/VM-DrivenWizard/ViewModels/WelcomePageViewModel.vb
@@ -1,13 +1,9 @@
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.POCO
 
 Namespace VM_DrivenWizard.ViewModels
+
     Public Class WelcomePageViewModel
         Inherits WizardViewModelBase
         Implements ISupportWizardNextCommand
@@ -16,19 +12,23 @@ Namespace VM_DrivenWizard.ViewModels
             ShowCancel = True
             ShowNext = True
         End Sub
+
         Public Shared Function Create() As WelcomePageViewModel
             Return ViewModelSource.Create(Function() New WelcomePageViewModel())
         End Function
-        Public ReadOnly Property CanGoForward() As Boolean Implements ISupportWizardNextCommand.CanGoForward
+
+        Public ReadOnly Property CanGoForward As Boolean Implements ISupportWizardNextCommand.CanGoForward
             Get
                 Return True
             End Get
         End Property
+
         Public Sub OnGoForward(ByVal e As CancelEventArgs) Implements ISupportWizardNextCommand.OnGoForward
             GoForward()
         End Sub
+
         Protected Sub GoForward()
-            Me.GetRequiredService(Of IWizardService)().Navigate("PlayTunePage", Model, Me)
+            GetRequiredService(Of IWizardService).Navigate("PlayTunePage", Model, Me)
         End Sub
     End Class
 End Namespace
