def506577b56ff72bf998428ff1a87bda7f6643d
DevExpressExampleBot <fakeEmail>
2025-07-15 15:47:28 +0400

    autogenerate VB

---
+++ VB/VM-DrivenWizard/MainWindow.xaml.vb
@@ -0,0 +1,13 @@
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace VM_DrivenWizard
+
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
